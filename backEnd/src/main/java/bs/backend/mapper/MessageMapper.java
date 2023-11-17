1e17263b9eff1063ad5d500f70bc2913c0ef47f6
PeiPei233 <2503585731@qq.com>
2023-11-17 19:08:40 +0800

    Add new methods to MessageMapper interface and corresponding SQL queries

--- backEnd/src/main/java/bs/backend/mapper/MessageMapper.java
+++ backEnd/src/main/java/bs/backend/mapper/MessageMapper.java
@@ -1,7 +1,21 @@
 package bs.backend.mapper;
 
+import bs.backend.model.MessageCount;
+import bs.backend.model.Message;
+import bs.backend.model.MessageInfo;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface MessageMapper {
+
+    void insertMessage(Message message);
+    Integer getMessageStatusCount(Integer uid, Integer status);
+    Integer getMessageStatusCountByTime(Integer uid, Integer status, Long beginTime, Long endTime);
+    List<MessageInfo> getMessages(Integer uid, Long beginTime, Long endTime);
+    List<MessageInfo> getMessagesByDid(Integer uid, Integer did, Long beginTime, Long endTime);
+    List<MessageInfo> getDeviceLatestEach(Integer uid);
+    List<MessageCount> getMostMessageDevices(Integer uid);
+
 }
